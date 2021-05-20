3fb0d4a3c454169cfae50585c258ee423369d3cf
Nassim Essaidi <contact@essaidi.me>
2021-05-20 01:42:11 +0100

    another method to render a template

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
-from polls.models import Question
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import loader
 
+
+from polls.models import Question
 # Create your views here.
 
 
 def index(request):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
-    # return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
     context = {
-        'lastest_question_list': lastest_question_list
+        'lastest_question_list': lastest_question_list,
     }
-    return render(request, 'polls/index.html', context)
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
