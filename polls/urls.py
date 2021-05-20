3875395ad930c6970dd9b40b2422120f356e5d28
Nassim Essaidi <contact@essaidi.me>
2021-05-20 02:19:38 +0100

    Namespacing URL names

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# we specify app_name so Django knows which app view to create for a url when using the {% url %} template tag
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('specifics/<int:question_id>/', views.detail, name="detail"),
